7a6f6318575107875b9f63fe59d0d966781b65cc
Yuzbekov Elvin <yuzbekov79@gmail.com>
2023-01-29 14:39:23 +0400

    counter

---
+++ main.js
@@ -0,0 +1,28 @@
+const counter = document.querySelector('#counter');
+const btns = document.querySelectorAll('.btn');
+
+let count = 0;
+
+btns.forEach((btn) => {
+    btn.addEventListener('click', myFunc);
+})
+
+function myFunc(e) {
+    const styles = e.currentTarget.classList
+    if (styles.contains('increase')) {
+        count++
+    } else if (styles.contains('decrease')) {
+        count--
+    } else {
+        count = 0
+    }
+
+    if(count>0){
+        counter.style.color='green'
+    }else if(count<0){
+        counter.style.color='red'
+    }else{
+        counter.style.color='gray'
+    }
+    counter.textContent = count
+}
